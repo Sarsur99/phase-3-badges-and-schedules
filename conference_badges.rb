1c98bcceb1ef041488de156767d3a2a7e809d75d
Soren <sorenisme@gmail.com>
2022-11-06 15:42:58 -0500

    commit tree edad1a561dd78c117b73178eed07a978bf184cbd

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,34 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(array)
+    newArray = []
+    array.each do|name|
+        newArray << "Hello, my name is #{name}."
+    end
+    newArray
+end
+
+
+def assign_rooms(nameArray)
+    newArray = []
+    roomIndex = 0
+    nameArray.each do |name|
+        newArray << "Hello, #{name}! You'll be assigned to room #{roomIndex += 1}!"
+    end
+    newArray
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |badge|
+      puts badge
+    end
+  
+    assign_rooms(attendees).each do |assignment|
+      puts assignment
+    end
+end
